1886e392b9a268872912378cb742349e372f48ca
fiorella frini <fiorella_frini@hotmail.com>
2022-12-16 17:35:13 -0300

    React 01

---
+++ 06-React-Intro/homework/01 - Exercises/src/components/Bienvenido.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import Botones from "./Botones";
+
+const studentName = "Fiorella Frini";
+const techSkills = ["Html", "Css", "JavaScript", "React", "Redux"];
+const alerts = { m1: "Aprobado", m2: "En curso" };
+
+export default function Bienvenido() {
+  // el código de tu componente acá
+  return(
+    <div>
+      <h1>Homework 1 de React</h1>
+      <h3>{studentName}</h3>
+      <ul>
+       {
+         techSkills.map((str,index)=>{
+          return <li key={index}>{str}</li>
+         })
+       }
+      </ul>
+      
+      <Botones alerts={alerts} />
+    
+    </div>
+  )
+}
+
+// Esto lo exportamos para los tests
+export { studentName, techSkills, alerts };
